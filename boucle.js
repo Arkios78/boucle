0cfb6f8be269e210fb7dee2e58d8dff0f0ac48f7
Arkios78 <145431240+Arkios78@users.noreply.github.com>
2023-09-22 12:01:20 +0200

    Add files via upload

---
+++ boucle.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/**            BOUCLE
+ *              FOR
+ * Declarer une variable truc qui est egale à un entier positif
+ * 
+ * Faire un console log de 'OUAIS :)' autant de fois que le valeur de truc
+ * 
+ * 
+ */
+
+let truc=4;
+for(let i=0;i<truc;i++){
+    console.log('OUAIS :)');
+}
